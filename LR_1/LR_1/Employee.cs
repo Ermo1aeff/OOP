f78629a279cee59f5a82c3177fe42b786548fe5b
Egor <94222033+Ermo1aeff@users.noreply.github.com>
2024-09-11 18:01:57 +0300

    Lab1 Added

---
+++ LR_1/LR_1/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_1
+{
+    // Класс Сотрудник
+    abstract class Employee
+    {
+        public required int Id { get; set; } // Табельный номер
+        public string? LastName { get; set; } // Фамилия
+        public string? FirstName { get; set; } // Имя
+        public string? MiddleName { get; set; } // Отчество
+        public string? Phone { get; set; } // Телефон
+        public string? Email { get; set; } // Email
+    }
+
+    // Класс Кладовщик
+    class Storekeeper : Employee
+    {
+
+    }
+
+    // Класс Менеджер
+    class Manager : Employee
+    {
+
+    }
+
+    class MateriallyResponsiblePerson : Employee
+    {
+
+    }
+
+    class Warehouse
+    {
+        public Storekeeper Storekeeper { get; set; }
+    }
+}
---
+++ LR_1/LR_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using LR_1;
+using System;
+using System.Collections.Generic;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    // Связь "один ко многим" с классом Book
+    public List<Book> Books { get; set; } = new List<Book>();
+}
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    // Связь с классом Author
+    public Author Author { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создание автора
+        Author author = new Author { Id = 1, Name = "Александр Пушкин" };
+
+        // Создание книг
+        Book book1 = new Book { Id = 1, Title = "Евгений Онегин", Author = author };
+        Book book2 = new Book { Id = 2, Title = "Руслан и Людмила", Author = author };
+
+        // Добавление книг к автору
+        author.Books.Add(book1);
+        author.Books.Add(book2);
+
+        // Вывод информации
+        Console.WriteLine($"Автор: {author.Name}");
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine($"Книга: {book.Title}");
+        }
+
+        Storekeeper storekeeper = new() { Id = 1 };
+        Manager manager = new() { Id = 1 };
+        MateriallyResponsiblePerson materiallyResponsiblePerson = new() { Id = 1 };
+
+        storekeeper.FirstName = "Василий";
+        manager.FirstName = "Коля";
+        manager.FirstName = "Всетлана";
+
+    }
+}
